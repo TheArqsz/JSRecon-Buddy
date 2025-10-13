ef2672b11a299e8b01a3ce6c5746e168a55bb506
TheArqsz <38382850+TheArqsz@users.noreply.github.com>
2025-10-13 23:32:17 +0200

    test: Added new manual test for nextjs manifest analysis

---
+++ assets/manual-tests/nextjs-manifest-test/_next/static/fake-build-id-12345/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "/": ["static/chunks/pages/index.js"],
+  "/404": ["static/chunks/pages/404.js"],
+  "/about-us": ["static/chunks/pages/about-us.js"],
+  "/admin/dashboard": ["static/chunks/pages/admin/dashboard.js"],
+  "/products/[slug]": ["static/chunks/pages/products/[slug].js"],
+  "/_app": ["static/chunks/pages/_app.js"],
+  "sortedPages": [
+    "/",
+    "/404",
+    "/about-us",
+    "/admin/dashboard",
+    "/products/[slug]",
+    "/_app"
+  ]
+};
